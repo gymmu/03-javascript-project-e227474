81d5955404e0eb1e2d3b04cb41c4257dd234eef8
Nikolay <e227474@edu.sbl.ch>
2023-10-31 13:28:11 +0100

    aufgabe 1-4 gelöst

--- script.js
+++ script.js
@@ -4,7 +4,65 @@ export function aufgabe01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
+    if(currentElement==="e"){
+      result.push("_")
+    }
+    else if(currentElement==="E") {
+      result.push("3")
+
+    }
+    else{
+      result.push(currentElement)
+    }
+  }
+  return result.join("")
+}
+
+ export function aufgabe02(args) {
+  const input = args
+  const result = []
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    result.push(currentElement.toUpperCase())
+  }
+ return result.join("")
+}
+
+export function aufgabe03(args) {
+  const input = args
+  const result = []
+  var ecount = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if(currentElement==="e"|currentElement==="E"){
+      ecount ++
+    }
+  }
+  return ecount
+}
+export function aufgabe04 (args) {
+  const input = args
+  const result = []
+  var wordcount = 1
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if(currentElement===" "){
+      wordcount++
+    }
+  }
+  return wordcount
+}
+
+export function aufgabe05 (args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if(currentElement.isUppercase()){
+    }
   }
   return result.join("")
 }
