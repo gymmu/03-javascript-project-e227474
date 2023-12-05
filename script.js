208c4f9b5b5e61f93a918d1eceff55a24dba7db3
Nikolay <e227474@edu.sbl.ch>
2023-12-05 11:05:54 +0100

    aufg 1-6 werden als korrekt angezeigt

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ export function aufgabe01(args) {
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
     if(currentElement==="e"){
-      result.push("_")
+      result.push("")
     }
     else if(currentElement==="E") {
-      result.push("3")
+      result.push("")
 
     }
     else{
@@ -52,17 +52,68 @@ export function aufgabe04 (args) {
     if(currentElement===" "){
       wordcount++
     }
+    else if(currentElement==="-"){
+      wordcount--
+    }
   }
   return wordcount
 }
 
 export function aufgabe05 (args) {
   const input = args
-  const result = []
+  
   for (let i = 0; i < input.length; i++) {
+    const currentAscii = input[i].charCodeAt(0)
+   
+    if (65 <= currentAscii && currentAscii <=90) return true
+    
+  }
+  return false
+}
+
+
+export function aufgabe06(args) {
+  const input = args
+
+  if (input.length <= 0) return false
+  
+  for (let i = 0; i < input.length; i++) {
+    const ascii = input[i].charCodeAt(0)
+
+    if(0 <= ascii && ascii <=31) return true
+    else if (33 <= ascii && ascii <= 47) return true
+    else if (58 <= ascii && ascii <= 64) return true
+    else if (91 <= ascii && ascii <= 96) return true
+    else if (123 <= ascii && ascii <= 127) return true
+  
+  }
+  return false
+}
+
+export function aufgabe10 (args) {
+  const input = args
+  if (input.length !== 7) return false
+  if(input[0] !== "#") return false
+
+  for (let i = 1; i < input.length; i++) {
     const currentElement = input[i]
-    if(currentElement.isUppercase()){
+    const ascii = currentElement.charCodeAt(0)
+    if (48 <= ascii && ascii <= 57){
+      // ist eine Ziffer
+    } else if(65 <= ascii && ascii <= 70){
+      // ist A-F
+    } else{
+      return false
     }
   }
-  return result.join("")
+  return true
+}
+
+export function aufgabe11 (args) {
+  const input = args
+  const result = []
+  const currentElement = input[i]
+  const ascii = currentElement.charCodeAT(0)
+  result = ascii
+  return result
 }
