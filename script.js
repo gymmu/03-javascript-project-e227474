5d8400d682e97e7b7be269dd2fbfbfcea5a4e7c2
Nikolay <e227474@edu.sbl.ch>
2023-12-05 11:18:44 +0100

    aufgabe 1-14 ohne 7 korrigiert und funktionieren

--- script.js
+++ script.js
@@ -71,7 +71,6 @@ export function aufgabe05 (args) {
   return false
 }
 
-
 export function aufgabe06(args) {
   const input = args
 
@@ -89,31 +88,103 @@ export function aufgabe06(args) {
   }
   return false
 }
+export function aufgabe08(args) {
+  const input = args        
+  const result = []                     
 
-export function aufgabe10 (args) {
-  const input = args
-  if (input.length !== 7) return false
-  if(input[0] !== "#") return false
-
-  for (let i = 1; i < input.length; i++) {
-    const currentElement = input[i]
-    const ascii = currentElement.charCodeAt(0)
-    if (48 <= ascii && ascii <= 57){
-      // ist eine Ziffer
-    } else if(65 <= ascii && ascii <= 70){
-      // ist A-F
-    } else{
-      return false
+for (let i = 0; i < input.length; i++) {      
+    const currentElement = input[i]        
+    if (currentElement === "e") {   
+      result.push(3)                                                           
+    } else {                                                 
+      result.push(currentElement)
     }
   }
+  return result.join("")
+}
+
+export function  aufgabe09 (args) {
+  const input = args
+  const result = []
+  let len = 0
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    len++
+  }
+  if (len === 6)  {
+    return true
+  }
+  return false
+  }
+ 
+export function aufgabe10 (args) {
+ const input = args
+
+ if(input.length !== 7) return false
+ if(input[0] !== "#") return false
+ for (let i = 1; i < input.length; i++) {
+   const currentElement = input[i]
+   const ascii = currentElement.charCodeAt(0)
+ 
+  if(48 <= ascii && ascii <=57) {
+       
+   } else if (65 <= ascii && ascii <= 70) {
+        
+   } else {
+     return false
+   }
+ }
+  
   return true
 }
 
-export function aufgabe11 (args) {
+export function  aufgabe11 (args) {
+const input = args
+
+if(input.length !== 1) return null
+  return input[0].charCodeAt(0)
+}
+
+export function aufgabe12 (args) {
   const input = args
   const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if(currentElement == "e"){
+      return i
+    }
+  }
+  return -1
+}
+
+export function aufgabe13 (args) {
+  const input = args
+  let posE = -1
+  for (let i = 0; i < input.length; i++) {
   const currentElement = input[i]
-  const ascii = currentElement.charCodeAT(0)
-  result = ascii
-  return result
+    if(currentElement === "e") {
+      posE = i
+    }
+  }
+  return posE
+ }
+
+ export function aufgabe14 (args) {
+  const input = args
+ let Count = 0
+  let posE= -1 
+  for (let i = 0; i < input.length; i++) {
+  const currentElement = input[i]
+
+
+
+  if(currentElement=== "e"){
+   Count++
+   if (Count === 3) {
+    return i
+   }
+  }
+}
+
+  return posE 
 }
